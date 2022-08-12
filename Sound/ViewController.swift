5ee8378de7f5f92168b29e5b200f5731df101eea
HiromasaHori <horihiromasa.tech@gmail.com>
2022-08-12 15:55:57 +0900

    サウンドアプリ2完了

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -10,17 +10,26 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    @IBOutlet var drumButton: UIButton!
+    
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func tapDrumButton(){
+    @IBAction func touchDownDrumButton(){
+        
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
+        
         drumSoundPlayer.currentTime = 0
         
         drumSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpDrumButton(){
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
     }
 }
 
